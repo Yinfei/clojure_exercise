b122dec379f0feaa5621326d98ecc2236fe4bd87
Yohan Piron <Yinfei84@gmail.com>
2015-03-04 16:50:35 +0100

    32 cards deck shouldn't be able tu pick jokers

--- src/rpg_tools_clj/card.clj
+++ src/rpg_tools_clj/card.clj
@@ -1,6 +1,6 @@
 (ns rpg-tools-clj.card)
 
-(def deck-of-54-cards (flatten [(range 1 11 1), "Jack", "Queen", "King"]))
+(def deck-of-52-cards (flatten [(range 1 11 1), "Jack", "Queen", "King"]))
 
 (def deck-of-32-cards (flatten [(range 7 11 7), "Jack", "Queen", "King"]))
 
@@ -11,8 +11,8 @@
   []
   (println "Joker"))
 
-(defn pick-from-54-deck
-  "It pick a card from a 54 cards deck with two Jokers"
+(defn pick-from-52-deck
+  "It pick a card from a 52 cards deck with two Jokers"
   []
   (if (< (rand-int 55) 2)
     (pick-joker)
@@ -27,6 +27,6 @@
   "It pick a card from a deck"
   [type]
   (condp <= type
-    54 (pick-from-54-deck)
+    52 (pick-from-52-deck)
     32 (pick-from-32-deck)
-    (throw (Exception. "Invalid argument. Usage: pick-card [32 64]"))))
+    (throw (Exception. "Invalid argument. Usage: pick-card [32 52]"))))
